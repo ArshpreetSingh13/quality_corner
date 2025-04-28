e4e2dc6bfa4599a8c166348692c00a6ef6b2aff7
Arshpreet singh <arshpreetsingh1327@gmail.com>
2025-04-28 11:05:10 +0530

    comments

--- admin_heading.php
+++ admin_heading.php
@@ -8,48 +8,38 @@
     <meta content="" name="keywords">
     <meta content="" name="description">
 
-    <!-- Google Web Fonts -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap"
         rel="stylesheet">
 
-    <!-- Icon Font Stylesheet -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    <!-- Libraries Stylesheet -->
+
     <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
 
-    <!-- Customized Bootstrap Stylesheet -->
+    
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
+    
+    
     <link href="css/style.css" rel="stylesheet">
     <link href="css/add.css" rel="stylesheet">
 </head>
 
 <body>
 
-
-
-
-    <!-- Spinner Start -->
     <div id="spinner"
         class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
         <div class="spinner-grow text-primary" role="status"></div>
     </div>
-    <!-- Spinner End -->
 
-
-
-   
-
-
-    <!-- Navbar start -->
     <div class="container-fluid sticky-top bg-white mt-2 A">
     
         <div class="container  topbar bg-primary d-none d-lg-block">
@@ -82,9 +72,6 @@
                     <div class="navbar-nav mx-auto">
                         <a href="index.php" class="nav-item nav-link">Home</a>
                         
-                        
-                        
-
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle " data-bs-toggle="dropdown">Category</a>
                             <div class="dropdown-menu m-0 bg-secondary rounded-0">
@@ -99,8 +86,6 @@
                             <div class="dropdown-menu m-0 bg-secondary rounded-0">
                                 <a href="add_product.php" class="dropdown-item">Add Product</a>
                                 <a href="manage_product.php" class="dropdown-item">Manage Product</a>
-                                
-                                
                                 
                             </div>
                         </div>
@@ -118,26 +103,13 @@
                             <a href="#" class="nav-link dropdown-toggle " data-bs-toggle="dropdown">More...</a>
                             <div class="dropdown-menu m-0 bg-secondary rounded-0">
                                
-                                
                                  <a href="Requsts.php" class="dropdown-item">Requsts</a>
                                 <a href="manage_order.php" class="dropdown-item">Orders</a>
-                                
-                                
+
                             </div>
                         </div>
 
-                       
-                           
-                       
-                           
                            <a href='Logout.php' class='nav-item nav-link'>Logout</a>
-                       
-
-                       
-                        
-
-                       
-
 
                     </div>
                     <div class="d-flex m-3 me-0">
@@ -152,4 +124,3 @@
             </nav>
         </div>
     </div>
-    <!-- Navbar End -->
